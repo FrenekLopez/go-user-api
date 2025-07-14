cb457cc151ec8eb18a8ef3231735e8b27bf4f98b
Frenek Lopez <eric.frenek.lr@gmail.com>
2025-07-13 23:49:08 -0700

    Siguiente Commit

---
+++ cmd/week1/factorial.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
+
+func main() {
+	n := 10
+	fmt.Printf("Factorial de %d: %d\n", n, factorial(n))
+}
